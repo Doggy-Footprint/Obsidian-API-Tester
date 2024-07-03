2b376d4a55de55fb03c51c6b1a6566e4277b87e3
Concepty <khs1903b@gmail.com>
2024-07-03 20:04:03 +0900

    update: cleanup event testing files

--- main.ts
+++ main.ts
@@ -21,32 +21,96 @@ export default class MyPlugin extends Plugin {
                 console.log(context);
             });
         }
+        // tests about TFiles, TFolders
         if (true) {
-            const rename_test_file = this.app.vault.getFileByPath("rename_test.md");
-            if (rename_test_file && true) {
-                this.registerEvent(this.app.vault.on('rename', (rename_test_2_file, oldPath) => {
-                    if (!(rename_test_2_file instanceof TFile)) return;
-                    console.log('rename_test_file === rename_test_2_file: ', rename_test_file === rename_test_2_file);
-                    console.log('rename_test_file object basename: ', rename_test_file.basename);
-                    console.log('rename_test_2_file object basename: ', rename_test_2_file.basename);
-                    console.log('----');
-                }));
-                // rename rename_test.md -> rename_test_2.md
-                /**
-                 * result - same object
-                 * rename_test_file === rename_test_2_file:  true
-                 * rename_test_file object basename:  rename_test_2
-                 * rename_test_2_file object basename:  rename_test_2
-                 */
-            } else if (false) {
-                // testing rename event of plugin.app.vault
-                this.registerEvent(this.app.vault.on('rename', (file, oldPath) => {
-                    console.log(file);
-                    console.log(oldPath);
-                    console.log('---');
-                }));
+            const DELETE_FIRST = true;
+            const CREATE_FIRST = true;
+
+            console.log('Cleaning up testing files and check some behaviors')
+            const event_test_dir = this.app.vault.getFolderByPath('event_test');
+            if (event_test_dir && DELETE_FIRST) {
+                console.log('event_test exists');
+                this.app.vault.delete(event_test_dir, true);
+                console.log('deleted directory ---')
+                console.log(event_test_dir); // still exists after deletion
+                console.log('---------');
+                // test if deleted object is used
+                // result: not related
+                if (false && !CREATE_FIRST) {
+                    const new_event_test_dir = await this.app.vault.createFolder('event_test');
+                    console.log('are they same objects?');
+                    console.log(`event_test_dir == new_event_test_dir : ${event_test_dir == new_event_test_dir}`);
+                    console.log(`event_test_dir === new_event_test_dir : ${event_test_dir === new_event_test_dir}`);
+                    console.log('---------');
+                }
+            } else {
+                console.log("can't find event_test");
             }
 
+            const event_test_file = this.app.vault.getFileByPath('event_test.md');
+            if (event_test_file && DELETE_FIRST) {
+                console.log('event_test file exists');
+                this.app.vault.delete(event_test_file, true);
+                console.log('deleted file ---')
+                console.log(event_test_file); // still exists after deletion
+                console.log('---------');
+                // test if deleted object is used
+                // result: not related
+                if (false && !CREATE_FIRST) {
+                    const new_event_test_file = await this.app.vault.create('event_test.md', 'abc');
+                    console.log('are they same objects?');
+                    console.log(`event_test_dir == new_event_test_dir : ${event_test_file == new_event_test_file}`);
+                    console.log(`event_test_dir === new_event_test_dir : ${event_test_file === new_event_test_file}`);
+                    console.log('---------');
+                }
+            } else {
+                console.log("can't find event_test_file");
+            }
+            /**
+             * events: create, modify, delete, event
+             * event_test/                 
+             * └── common_dir/              check how renaming common_dir affect sub files.
+             *     ├── test1/               
+             *     │   └── test1.md         
+             *     ├── test2/               
+             *     │   └── test2.md         
+             *     ├── TFolder_object/      check if modify, event event changes TFolder object (reference)
+             *     └── TFile_object         check if modify, event event changes TFile object (reference)
+             * event_test.md
+            */
+            console.log('creating testing files');
+            if (DELETE_FIRST && CREATE_FIRST) {
+                await this.app.vault.createFolder('event_test');
+                await this.app.vault.createFolder('event_test/common_dir');
+                await this.app.vault.createFolder('event_test/common_dir/test1');
+                await this.app.vault.create('event_test/common_dir/test1/test1.md', 'test1');
+                await this.app.vault.createFolder('event_test/common_dir/test2');
+                await this.app.vault.create('event_test/common_dir/test2/test2.md', 'test2');
+                await this.app.vault.createFolder('event_test/common_dir/TFolder_object');
+                await this.app.vault.create('event_test/common_dir/TFile_object.md', 'tfile object');
+                await this.app.vault.create('event_test.md', 'event_test');
+                console.log('created test files');
+            }
+
+            // TODO: what is `ctx?: any` on this.app.vault.on()?
+            const watch_TFolder_object = this.app.vault.getFileByPath("event_test/common_dir/TFolder_object");
+            const watch_TFile_object = this.app.vault.getFileByPath("event_test/common_dir/TFile_object");
+
+            this.registerEvent(this.app.vault.on('create', obj => {
+
+            }));
+
+            this.registerEvent(this.app.vault.on('modify', obj => {
+
+            }));
+
+            this.registerEvent(this.app.vault.on('delete', obj => {
+
+            }));
+
+            this.registerEvent(this.app.vault.on('rename', (obj, oldPath) => {
+
+            }));
         }
     }
 
